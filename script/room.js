2681ab7ec95c6ccfdc39e58844d8c9e82756b9e4
asent123 <42821292+asent123@users.noreply.github.com>
2018-09-04 17:52:38 -0700

    Update room.js

--- script/room.js
+++ script/room.js
@@ -15,14 +15,14 @@ var Room = {
 		'trap': {
 			name: _('trap'),
 			button: null,
-			maximum: 100,
+			maximum: 10000,
 			availableMsg: _('builder says she can make traps to catch any creatures might still be alive out there'),
 			buildMsg: _('more traps to catch more creatures'),
 			maxMsg: _("more traps won't help now"),
 			type: 'building',
 			cost: function() {
 				return {
-					'wood': 1
+					'wood': 0
 				};
 			}
 		},
